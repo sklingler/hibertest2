42591d925d3b55c8ffd3f207919eccd22a110976
sean <sklingler@gmail.com>
2016-02-25 15:00:31 -0500

    query results analyzed and proved that we can make JSON

--- src/main/java/hibertest/HiberQTest.java
+++ src/main/java/hibertest/HiberQTest.java
@@ -1,11 +1,20 @@
 package hibertest;
 
 import java.io.File;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.engine.query.spi.sql.NativeSQLQueryReturn;
+import org.hibernate.type.Type;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class HiberQTest {
 
@@ -41,6 +50,8 @@ public class HiberQTest {
 		 * 
 		 * t.commit();//transaction is committed
 		 */
+
+		performQuery(session);
 		session.close();
 
 		log.info("successfully saved");
@@ -56,4 +67,50 @@ public class HiberQTest {
 		}
 	}
 
+	private void performQuery(Session session) {
+		// SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		// Session session = sessionFactory.getCurrentSession();
+
+		// Get All Employees
+		// Transaction tx = session.beginTransaction();
+		log.info("entered performQuery");
+//		SQLQuery query = session.createSQLQuery("select * from aoproducts");
+		 SQLQuery query = session.createSQLQuery("select id J4FrankId, name J4FrankName, startdate J4FrankDate, isinactiveflag J4FrankIsInactive from aoproducts");
+		List<Object[]> rows = query.list();
+
+		 query.setResultTransformer(Criteria.ALIAS_TO_ENTITY_MAP);
+         List data = query.list();
+
+         for(Object object : data)
+         {
+            Map row = (Map)object;
+            System.out.print("id: " + row.get("id")); 
+            System.out.println(", name: " + row.get("name")); 
+         }
+		 /*
+		 * Type types[] = query.getReturnTypes(); for(Type aType: types){
+		 * log.info("returned type: " + aType); }
+		 */
+
+		/*
+		 * String aliases[] = query.getReturnAliases(); log.info(
+		 * "got aliases: " + aliases);
+		 */
+		log.info("got " + rows.size() + " rows");
+
+		for (Object[] rowEntity : rows) {
+			log.info("row start");
+			for (Object entityCol : rowEntity) {
+				log.info(" " + entityCol);
+			}
+		}
+
+		Gson gson = new Gson();
+//		String json = gson.toJson(rows);
+		String json = gson.toJson(data);
+		log.info("created JSON " + json);
+
+		log.info("exited performQuery");
+
+	}
 }
